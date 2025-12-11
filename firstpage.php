b1d48380184d0a26974b40099c09922dc9d528cd
67219010009-cmyk <67219010009@cvc-cha.ac.th>
2025-12-11 13:46:54 +0700

    Add files via upload

---
+++ firstpage.php
@@ -0,0 +1,24 @@
+<?php
+echo "บุญญฤทธิ์ พีภูเขียว";
+echo "<br>";
+echo '1524568454';
+echo " อายุ \n";
+echo 20;
+$age=23;
+print $age;
+print "$age";
+echo "sum of 5+10:", 5+10;
+echo "<br> sum 5+10 :", 5+10;
+echo "<br> บุญญฤทธิ์ พีภูเขียว". '1524568454';
+echo "<br> score sum 5+10 :", 5+10, "score";
+$height = 170;
+$sumah= $height + $age;
+echo "<br>ส่วนสูง+อายุ: ".$height+$age." ซม.";
+$school="วิทยาลัยอาชีวศึกษาฉะเชิงเทรา";
+$address="99/9 ม.6 ต.บางพระ อ.เมือง จ.ฉะเชิงเทรา 2";
+echo "<br>โรงเรียน: ".$school." ".$address;
+$addr = "120/1 ม.15 ";
+echo "<br>ที่อยู่: " . $addr;
+$addr .= "ต.ท่าไข่ อ.เมืองฉะเชิงเทรา จ.ฉะเชิงเทรา";
+echo "<br>ที่อยู่เต็ม: " . $addr;
+?>
